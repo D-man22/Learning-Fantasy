a3282a3564ff1666efe5cc7d4b69346f0ea26d52
D-man22 <160277391+D-man22@users.noreply.github.com>
2024-02-18 17:56:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ import mysql.connector
 formDataLocal = cgi.FieldStorage()
 
 
-#connection = sqlite3.connect('app.db', check_same_thread=False)
-connection = mysql.connector.connect(
-  host="mysql-rds-noted-cherry-pz2e.cjaeoe84erpr.ca-central-1.rds.amazonaws.com",
-  user="root",
-  password="DjMqeN1ePD7NHQ2A",
-  database="primarydb"
-)
+connection = sqlite3.connect('app.db', check_same_thread=False)
+# connection = mysql.connector.connect(
+#   host="mysql-rds-noted-cherry-pz2e.cjaeoe84erpr.ca-central-1.rds.amazonaws.com",
+#   user="root",
+#   password="DjMqeN1ePD7NHQ2A",
+#   database="primarydb"
+# )
 
 sql = connection.cursor()
 
