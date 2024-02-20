8f27fe8cfa1506cf4c4ece17dfb10203230b7f40
D-man22 <160277391+D-man22@users.noreply.github.com>
2024-02-20 15:35:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -50,8 +50,14 @@ def getdb():
             ssl_ca='SSL/certs/ca-cert.pem'
         )
     return g.db
-
 db = getdb()
+
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None and db.is_connected():
+        db.close()
+
+current_app.teardown_appcontext(close_db)
 
 sql = db.cursor(dictionary=True)
 
