dda052c2be69e1c3da4e138c34e6a86b9a419b1b
D-man22 <160277391+D-man22@users.noreply.github.com>
2024-02-26 16:43:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ with app.app_context():
     def getdb():
         if 'db' not in g or not g.db.is_connected():
             g.db = mysql.connector.connect(
-                host=current_app.config['3306'],
+                host=current_app.config['mysql-rds-noted-cherry-pz2e.cjaeoe84erpr.ca-central-1.rds.amazonaws.com'],
                 user=current_app.config[user],
                 password=current_app.config[password],
                 database=current_app.config[database],
